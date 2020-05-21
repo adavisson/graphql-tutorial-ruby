f7dd61dbc4ba939d863408af67065647ab19c416
Andrew Davisson <akdavisson4@gmail.com>
2020-05-21 08:27:55 -0600

    generated link model

---
+++ db/migrate/20200521142653_create_links.rb
@@ -0,0 +1,10 @@
+class CreateLinks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.text :descrption
+
+      t.timestamps
+    end
+  end
+end
